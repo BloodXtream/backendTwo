1f41d5004d63f3b15940e0023e42c80dc75dab56
Pragyanam Tiwari <pragyanamtiwarii@gmail.com>
2025-07-15 20:26:46 +0530

    server with express

--- topicOne/server.js
+++ topicOne/server.js
@@ -1,9 +1,27 @@
-const http = require('http')
+// const http = require('http')
 
 
-const server = http.createServer((req, res) => {
-    res.end("hello new server response")
-}) // Server create ho gaya hai
-server.listen(3000, () => {
-    console.log(`Server is running on port 3000`)
+// const server = http.createServer((req, res) => {
+//     res.end("hello new server response")
+// }) // Server create ho gaya hai
+// server.listen(3000, () => {
+//     console.log(`Server is running on port 3000`)
+// })
+
+
+// Server with Express package
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to  page');
+})
+app.get('/home', (req, res) => {
+    res.send('Welcome to home page');
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
 })
