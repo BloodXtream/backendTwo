dffc4b35882ab0bb37d77197a4b0c1c7fcb5df48
Pragyanam Tiwari <pragyanamtiwarii@gmail.com>
2025-07-22 20:19:14 +0530

    Added middleware setup and routes

---
+++ 09 middleWares/src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const indexRouter = require("./routes/index.route");
+
+const app = express();
+app.use((req, res, next) => {
+    console.log('thi smiddle ware is between app and route');
+    next();
+})
+
+app.use('/', indexRouter);
+
+module.exports = app;
