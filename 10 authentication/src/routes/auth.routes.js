9eac1177aaeea6319d4b6a888f91384bb94100a8
Pragyanam Tiwari <pragyanamtiwarii@gmail.com>
2025-08-11 11:25:39 +0530

    learn and added jwt for authentication

--- 10 authentication/src/routes/auth.routes.js
+++ 10 authentication/src/routes/auth.routes.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
 const userModel = require('../models/user.model')
 
 const router = express.Router()
+require('dotenv').config()
 
 router.post('/register', async (req, res) => {
     const { username, password } = req.body
@@ -11,9 +13,16 @@ router.post('/register', async (req, res) => {
         password
     })
 
+    const token = jwt.sign(
+        { id: user._id },
+        process.env.JWT_SECRET
+    )
+
+    res.cookie("token", token)
+
     res.status(201).json({
         message: "user registered successfully",
-        user,
+        user
     })
 })
 
@@ -40,6 +49,32 @@ router.post('/login', async (req, res) => {
     res.status(200).json({
         message: "user loggedIn successfully"
     })
+})
+
+router.get('/user', async (req, res) => {
+    const { token } = req.body
+    if (!token) {
+        return res.statur(401).json({
+            message: "[Unauthorised]"
+        })
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const user = await userModel.findOne({
+            _id: decoded.id
+        }).select("-password -__v")
+
+        res.status(200).json({
+            message: "[Data fetched succesfully]",
+            user
+        })
+
+    } catch (err) {
+        return res.status(401).json({
+            message: "[Invalid token]"
+        })
+    }
 })
 
 
