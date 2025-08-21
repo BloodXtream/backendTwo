3c4528860b7da5c83e95bb8f7d9a96072349f6e2
Pragyanam Tiwari <pragyanamtiwarii@gmail.com>
2025-08-21 11:43:51 +0530

    register pagr UI done...

---
+++ 17 Chatgpt/frontend_backend/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const authRoutes = require('./routes/auth.routes');
+const indexRoutes = require('./routes/index.routes');
+const app = express();
+
+app.set("view engine", "ejs")
+app.use(express.static('public'))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/', indexRoutes)
+app.use('/auth', authRoutes)
+
+
+
+module.exports = app;
